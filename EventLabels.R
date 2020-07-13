12f76409dd4b4dc7476cb02d7cf38d58b8db4edc
Raj Dasani <rajrdasani@gmail.com>
2020-07-13 12:51:55 -0700

    Initial Push of NBA Tracking Functions

---
+++ EventLabels.R
@@ -0,0 +1,36 @@
+EVENTMSGTYPE
+# 1 - Make 
+# 2 - Miss 
+# 3 - Free Throw 
+# 4 - Rebound 
+# 5 - out of bounds / Turnover / Steal 
+# 6 - Personal Foul 
+# 7 - Violation 
+# 8 - Substitution 
+# 9 - Timeout 
+# 10 - Jumpball 
+# 12 - Start Q1? 
+# 13 - Start Q2?
+#   
+EVENTMSGACTIONTYPE
+# 1 - Jumpshot 
+# 2 - Lost ball Turnover 
+# 3 - ? 
+# 4 - Traveling Turnover / Off Foul 
+# 5 - Layup 
+# 7 - Dunk 
+# 10 - Free throw 1-1 
+# 11 - Free throw 1-2 
+# 12 - Free throw 2-2 
+# 40 - out of bounds 
+# 41 - Block/Steal 
+# 42 - Driving Layup 
+# 50 - Running Dunk 
+# 52 - Alley Oop Dunk 
+# 55 - Hook Shot 
+# 57 - Driving Hook Shot 
+# 58 - Turnaround hook shot 
+# 66 - Jump Bank Shot 
+# 71 - Finger Roll Layup 
+# 72 - Putback Layup 
+# 108 - Cutting Dunk Shot
